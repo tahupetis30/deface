6b0ffb453ac5069457d64b3193eef7fc28cc2067
tahupetis30 <80400375+tahupetis30@users.noreply.github.com>
2021-03-10 23:35:35 +0700

    Add files via upload

---
+++ tahu.php
@@ -0,0 +1,27 @@
+<?php
+system('clear');
+echo   "\n";
+echo   "_______________________\n";
+echo   "[+]   Menu  :\n";
+echo   "_______________________\n";
+echo   "[1]   Career resume file upload\n";
+echo   "[2]   Open journal system\n";
+echo   "[3]   Slims Mass Exploit\n";
+echo   "[4]   Keluar tuan\n";
+echo   "________________________\n";
+echo   "[+]   Pilih     :\n";
+echo   "\n    ==>  ";
+$pil     = trim(fgets(STDIN));
+if($pil     ==  1){
+      system('git clone https://github.com/Aldibima30/crfu');
+               }
+if($pil     ==   2){
+       system('https://github.com/Aldibima30/ojs-dir-scaner');
+               }
+if($pil     ==    3){
+       system('isisissss ajaaaa');
+                }
+if($pil     ==    4){
+       system('exit')
+                }
+>?
